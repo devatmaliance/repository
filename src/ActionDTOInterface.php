6c7f9f8ca05475bcb7d835cd15fd9fb2cbb68e5e
devatmaliance <94893319+devatmaliance@users.noreply.github.com>
2024-02-22 11:18:39 +0000

    updated action interface

--- src/ActionDTOInterface.php
+++ src/ActionDTOInterface.php
@@ -6,7 +6,7 @@ interface ActionDTOInterface
     public function getOperation(): string;
     public function getEntity(): string;
     public function getFields(): array;
-    public function setOperation(string $operation): string;
-    public function setEntity(string $entity): string;
-    public function setFields(array $action): string;
+    public function setOperation(string $operation): void;
+    public function setEntity(string $entity): void;
+    public function setFields(array $action): void;
 }
