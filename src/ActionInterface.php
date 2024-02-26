204b1313cb1e2f762527c964ebd9a2ff3d1c2879
devatmaliance <94893319+devatmaliance@users.noreply.github.com>
2024-02-26 14:04:47 +0000

    changed params in action interface

--- src/ActionInterface.php
+++ src/ActionInterface.php
@@ -4,7 +4,7 @@ namespace Devatmaliance\Repository;
 interface ActionInterface
 {
     public function create(array $fields): array;
-    public function update(array $fields): array;
-    public function delete(array $fields): array;
+    public function update(int $id, array $fields): array;
+    public function delete(int $id): array;
     public function getFields(): array;
 }
