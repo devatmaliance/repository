c4ec6b579ba158e4d671e788603045d9bfd07bd0
devatmaliance <94893319+devatmaliance@users.noreply.github.com>
2024-02-26 12:51:24 +0000

    updated action interface

--- src/ActionInterface.php
+++ src/ActionInterface.php
@@ -3,7 +3,8 @@
 namespace Devatmaliance\Repository;
 interface ActionInterface
 {
-    public function create(array $fields): void;
-    public function update(array $fields): void;
-    public function delete(array $fields): void;
+    public function create(array $fields): array;
+    public function update(array $fields): array;
+    public function delete(array $fields): array;
+    public function getFields(): array;
 }
