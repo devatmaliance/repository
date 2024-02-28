eee30c948c6f192757334fffa258cb6925e1ae11
devatmaliance <94893319+devatmaliance@users.noreply.github.com>
2024-02-28 04:57:19 +0000

    added new methods for ActionDTOInterface

--- src/ActionDTOInterface.php
+++ src/ActionDTOInterface.php
@@ -7,7 +7,9 @@ interface ActionDTOInterface
     public function getOperation(): string;
     public function getEntity(): string;
     public function getFields(): array;
+    public function getSearchParam(): string;
     public function setOperation(string $operation): void;
     public function setEntity(string $entity): void;
     public function setFields(array $fields): void;
+    public function setSearchParam(string $param): void;
 }
