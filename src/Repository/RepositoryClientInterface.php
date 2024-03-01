f59f128ac4891eaa39398287909717bf6236dbfc
devatmaliance <94893319+devatmaliance@users.noreply.github.com>
2024-03-01 10:00:06 +0000

    created respository yii

--- src/Repository/RepositoryClientInterface.php
+++ src/Repository/RepositoryClientInterface.php
@@ -2,6 +2,8 @@
 
 namespace Devatmaliance\Repository\Repository;
 
+use Devatmaliance\Repository\Action\ActionInterface;
+
 interface RepositoryClientInterface
 {
     public function send(ActionInterface $action);
---
+++ src/Repository/RepositoryServiceYii.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Devatmaliance\Repository\Service;
+
+use Devatmaliance\Repository\Action\ActionDTOInterface;
+use Devatmaliance\Repository\Repository\RepositoryEntityInterface;
+use Devatmaliance\Repository\Repository\RepositoryInterface;
+use Devatmaliance\Repository\Service\RepositoryServiceInterface;
+use Devatmaliance\Repository\Service\ServiceReceiverMapperDTOInterface;
+
+class RepositoryServiceYii implements RepositoryServiceInterface
+{
+    private ActionDTOInterface $actionDTO;
+    private RepositoryInterface $repository;
+    private RepositoryEntityInterface $repositoryEntity;
+    private ?ServiceReceiverMapperDTOInterface $mapperDTO;
+    private string $body;
+
+    public function __construct(
+        string $body,
+        RepositoryInterface  $repository,
+        ActionDTOInterface $actionDTO,
+        RepositoryEntityInterface $repositoryEntity,
+        ?ServiceReceiverMapperDTOInterface $mapperDTO = null
+    )
+    {
+        $this->body = json_decode($body, false);
+        $this->mapperDTO = $mapperDTO;
+        $this->repositoryEntity = $repositoryEntity;
+        $this->repository = $repository;
+        $this->actionDTO = $actionDTO;
+    }
+
+    /**
+     * @throws \Exception
+     */
+    public function execute(): array
+    {
+        $this->actionDTO->initDTO($this->body, $this->mapperDTO);
+        $method = $this->actionDTO->getOperation();
+        $this->repository->init($this->actionDTO, $this->repositoryEntity);
+        return $this->repository->$method();
+    }
+}
