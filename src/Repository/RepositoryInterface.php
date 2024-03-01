58c5217cba77a3b64a618fa5d3df73398446b160
devatmaliance <94893319+devatmaliance@users.noreply.github.com>
2024-03-01 10:35:05 +0000

    cadded method in repository interface

--- src/Repository/RepositoryInterface.php
+++ src/Repository/RepositoryInterface.php
@@ -2,9 +2,13 @@
 
 namespace Devatmaliance\Repository\Repository;
 
+use Devatmaliance\Repository\Repository\RepositoryEntityInterface;
+use Devatmaliance\Repository\Action\ActionDTOInterface;
+
 interface RepositoryInterface
 {
     public function create(): array;
     public function update(): array;
     public function delete(): array;
+    public function init(ActionDTOInterface $actionDTO, RepositoryEntityInterface $repositoryEntity): void;
 }
