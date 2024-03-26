841a9bfaaa2ac548be0d3fb2d35ddd55c6dbacce
devatmaliance <94893319+devatmaliance@users.noreply.github.com>
2024-03-26 08:02:09 +0000

    update repository yii

--- src/service/ServiceReceiverMapperDTOInterface.php
+++ src/service/ServiceReceiverMapperDTOInterface.php
@@ -5,7 +5,7 @@ namespace Devatmaliance\Repository\service;
 interface ServiceReceiverMapperDTOInterface
 {
     public function mapDtos(): array;
-    public function getMapDto(string $entity): array;
+    public function getFieldsMapDto(string $entity): array;
     public function getSearchParamsMapDto(string $entity, array $searchParams): array;
 
 }
